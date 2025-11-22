0589579f298a08229390de38c78c831b4fac4304
Muhammad Is'haq <bryanedwarding@gmail.com>
2025-11-22 04:16:15 +0100

    feat/created basic structures

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // astro.config.mjs
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
 });
